779ffe6a593eb86ed2303a0cdb88243f93de9d03
adil488444 <engadiljavedbhutta@gmail.com>
2025-09-04 19:32:29 +0500

    ma

--- app/src/main/java/com/example/personaltaskmanager/Model.kt
+++ app/src/main/java/com/example/personaltaskmanager/Model.kt
@@ -4,6 +4,10 @@ data class Category(
     val title: String,
     val location: String
 )
-data class CategoryResponse(
-    val categories: List<Category>
+data class Task(
+    val id: Int,
+    val title: String,
+    val description: String,
+    val location: String,
+    val isCompleted: Boolean
 )
